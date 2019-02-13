d6cf44abc59f5d4ec9009f3b305044bebf3e9f22
RonnyRitscher <r.k.ritscher@web.de>
2019-02-13 14:43:33 +0100

    asdasd

---
+++ app/src/main/java/com/example/myaktiehq/AktiendetailActivity.java
@@ -0,0 +1,38 @@
+package com.example.myaktiehq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class AktiendetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktiendetail);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_aktiendetail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings wurde gedrückt", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_profile:
+                Toast.makeText(this, "Profile wurde gedrückt", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return true;
+    }
+}
