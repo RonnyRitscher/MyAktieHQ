95a2aca70761fd1ce203ff4bab74de71d8115f19
RonnyRitscher <r.k.ritscher@web.de>
2019-02-14 15:08:05 +0100

    2019.02.14_15:17

--- app/src/main/java/com/example/myaktiehq/EinstellungenActivity.java
+++ app/src/main/java/com/example/myaktiehq/EinstellungenActivity.java
@@ -1,39 +1,44 @@
 package com.example.myaktiehq;
 
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 
-public class EinstellungenActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+public class EinstellungenActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Toast.makeText(this, "Einstellungen wurde gestartet", Toast.LENGTH_SHORT).show(); //KANN ENTFERNT WERDEN
-        addPreferencesFromResource(R.xml.preferences);          //!DEPRICATED
-        Preference aktienlistePref = findPreference(getString(R.string.preference_aktienliste_key)); // identifiziert den Key der Pref
-        aktienlistePref.setOnPreferenceChangeListener(this);    // übergibt den identifizierten Key an onPreferenceChange()
+//        //Toast.makeText(this, "Einstellungen wurde gestartet", Toast.LENGTH_SHORT).show(); //KANN ENTFERNT WERDEN
+//
+//        addPreferencesFromResource(R.xml.preferences);          //!DEPRICATED
+//        Preference aktienlistePref = findPreference(getString(R.string.preference_aktienliste_key)); // identifiziert den Key der Pref
+//        aktienlistePref.setOnPreferenceChangeListener(this);    // übergibt den identifizierten Key an onPreferenceChange()
+//
+//        // ...onPreferenceChange sofort aufrufen/anzeige mit der in SharedPreferences gespeicherten Aktienliste
+//        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this); // holt den default
+//        String gespeicherteAktienliste = sharedPrefs.getString(aktienlistePref.getKey(), ""); //
+//        onPreferenceChange(aktienlistePref, gespeicherteAktienliste); // ruft die Methode oPC mit Parametern auf
 
-        //-> onPreferenceChange sofort aufrufen/anzeige mit der in SharedPreferences gespeicherten Aktienliste
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this); // holt den default
-        String gespeicherteAktienliste = sharedPrefs.getString(aktienlistePref.getKey(), ""); //
+        // Hier binden wir das EinstellungsFragment in der Activity ein
+        // ...nicht den  getFragmentManager(), da wir über "extends AppCompatActivity" arbeiten
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, new EinstellungenFragment())
+                .commit();
 
-        onPreferenceChange(aktienlistePref, gespeicherteAktienliste); // ruft die Methode oPC mit Parametern auf
     }
 
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-        preference.setSummary(newValue.toString());             // Summary ist der wert/value von "preference_aktienliste_default"
-                                                                // ... da vom typ Objekt der Aufruf von toString()
-                                                                //... zeigt den eingetragenen Value (kleiner, unterhalb) des Keys an
-        return true;
-    }
+//    @Override
+//    public boolean onPreferenceChange(Preference preference, Object newValue) {
+//
+//        preference.setSummary(newValue.toString());             // Summary ist der wert/value von "preference_aktienliste_default"
+//                                                                // ... da vom typ Objekt der Aufruf von toString()
+//                                                                //... zeigt den eingetragenen Value (kleiner, unterhalb) des Keys an
+//        return true;
+//    }
 }
---
+++ app/src/main/java/com/example/myaktiehq/EinstellungenFragment.java
@@ -0,0 +1,35 @@
+package com.example.myaktiehq;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+
+public class EinstellungenFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
+
+    //eingefügt von EinstellungenActivity
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        preference.setSummary(newValue.toString());
+        return false;
+    }
+
+
+    @Override
+    public void onCreatePreferences(Bundle saveInstanceState, String rootKey) {
+        //!GEÄNDERT! addPreferencesFromResource(R.xml.preferences);          //!DEPRICATED
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+        Preference aktienlistePref = findPreference(getString(R.string.preference_aktienliste_key));    // identifiziert den Key der Pref
+        aktienlistePref.setOnPreferenceChangeListener(this);                                            // übergibt den identifizierten Key an onPreferenceChange()
+
+        //-> onPreferenceChange sofort aufrufen/anzeige mit der in SharedPreferences gespeicherten Aktienliste
+        //!GEÄNDERT! SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this); // holt den default
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());   // holt den default
+        String gespeicherteAktienliste = sharedPrefs.getString(aktienlistePref.getKey(), "");
+        onPreferenceChange(aktienlistePref, gespeicherteAktienliste);                                   // ruft die Methode oPC mit Parametern auf
+
+
+    }
+}
